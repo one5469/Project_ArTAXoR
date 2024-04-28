6fb17c10e74ba937b10983bcf568d5a7d3254bd0
one5469 <94108295+one5469@users.noreply.github.com>
2024-04-28 15:30:53 +0900

    Add files via upload

---
+++ CustomClass.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from torchvision.transforms import transforms, v2
+from torch.utils.data import Dataset, DataLoader
+
--- func_file.py
+++ func_file.py
@@ -8,6 +8,7 @@ import os
 
 baseTF = transforms.Compose(transforms=[
     transforms.Resize(size=(64, 64)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+    transforms.ToTensor()
+    # ,transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
 ])
+
